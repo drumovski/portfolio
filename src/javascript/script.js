00893cb1e52e015113792ebc69728b5d5cec2908
drumovski <jason.stacy@gmail.com>
2020-05-25 16:14:40 +1000

    Javascript comments

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,3 +1,4 @@
+// Javascript for drop-down of blog page content
 window.onload = function () {
   let coll = document.getElementsByClassName("collapsible");
   let i;
@@ -7,10 +8,10 @@ window.onload = function () {
       this.classList.toggle("active");
       let content = this.nextElementSibling;
       if (content.style.maxHeight) {
-        content.style.maxHeight = null;
-        content.style.paddingBottom = null;
+        content.style.maxHeight = null; // Hidden
+        content.style.paddingBottom = null; 
       } else {
-        content.style.maxHeight = "100%";
+        content.style.maxHeight = "100%"; // Revealed
         content.style.paddingBottom = "20px";
       }
     });
